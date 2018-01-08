2d90a8f0e8d722c1824bca0b3b935a59ac3471ce
1985IvanovAndrey <dron_krotova@i.ua>
2018-01-08 15:04:24 +0200

    add interval

--- Home28_12/src/Interval.java
+++ Home28_12/src/Interval.java
@@ -7,15 +7,6 @@ public class Interval{
         for(int i=a;i<=b;i++){
             System.out.print(i+" ");
         }
-        System.out.println();
-        while(a<=b){
-            System.out.print(a+" ");
-            a++;
-        }
-        System.out.println();
-        do {
-            System.out.print(d + " ");
-            d++;
-        }while(d<=b);
+
     }
 }
